98a38d89a591f1836e0d399bb5c45032a174bf14
kukunoorusvadrut <kukunoorusvadrut@outlook.com>
2016-04-22 17:36:45 -0700

    Update hello_app.py

--- hello_app.py
+++ hello_app.py
@@ -4,7 +4,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    # 10fastfingers.com
+# typing.lk
+# typing.com/games
+# typingclub.com
+# import the magic random picker
+
+# list of things to pick from
+typing_options = ['abc', 'def', 'ghi']
+
+# find a way to pick something
+typing_choice = typing_options[1]
+
+# a way to tell the user what to do
+print typing_choice
 
 if __name__ == "__main__":
     # go get the PORT from the environment
